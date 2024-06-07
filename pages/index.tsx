c92a1f2f1d0ccc8492c9509010c545259a9a9b89
Lolliiimor <rmorin@africaexchange.com>
2024-06-07 09:02:54 +0100

    final final

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,18 @@ import { MantineProvider } from "@mantine/core";
 
 export default function Home() {
   return (
-    <MantineProvider>  <main className="h-screen w-screen min-w-[1440px]">
-      <Header />
-      <RecipeSection />
-      <AboutUs />
-      <Community />
-      <CTA />
-      <Footer />
-    </main>
-  </MantineProvider>
-  )
+    <MantineProvider>
+      {" "}
+      <main className="h-screen  min-w-[1440px] justify-center  flex  ">
+        <div className="max-w-[1440px] flex  flex-col ">
+          <Header />
+          <RecipeSection />
+          <AboutUs />
+          <Community />
+          <CTA />
+          <Footer />
+        </div>
+      </main>
+    </MantineProvider>
+  );
 }
